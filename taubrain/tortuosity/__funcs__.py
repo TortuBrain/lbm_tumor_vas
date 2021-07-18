dc7189dfc6784839b63d7fae3c936df5a19125d5
Joseph avila <josdavil@espol.edu.ec>
2021-07-18 18:18:21 -0500

    add: : tortuosity streamlines tracking vascular :snake:

---
+++ taubrain/tortuosity/__funcs__.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def euclidian_distance_two_points(point_a, point_b) -> float:
+    distance = np.linalg.norm(point_a - point_b)
+    return distance
+
+
+def distance_points_array(points) -> float:
+    distance = 0
+    for position_point in range(len(points) - 1):
+        distance += euclidian_distance_two_points(
+            points[position_point], points[position_point + 1]
+        )
+    return distance
+
+
+def tortuosity_geometric_streamline(streamline_points) -> float:
+    distance_across = euclidian_distance_two_points(
+        streamline_points[0], streamline_points[-1]
+    )
+    return distance_points_array(streamline_points) / distance_across
+
+
+def tortuosity_geometric(streamlines) -> float:
+    length_streamlines = len(streamlines)
+    tau_geometric = 0
+    for streamline in streamlines:
+        if streamline.shape[0] > 1:
+            tau_geometric += tortuosity_geometric_streamline(streamline)
+    return tau_geometric / length_streamlines
