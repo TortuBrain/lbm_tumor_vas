b7cf32046383899100ea62cc1402df0ff8ac4f51
Joseph avila <josdavil@espol.edu.ec>
2021-07-22 17:10:11 -0500

    update:documentation tortuosity

--- taubrain/tortuosity/__funcs__.py
+++ taubrain/tortuosity/__funcs__.py
@@ -1,12 +1,38 @@
+# IMPORTS PACKAGES
 import numpy as np
 
 
 def euclidian_distance_two_points(point_a, point_b) -> float:
+    """
+    Find the Euclidean distance between two points
+    d(x,y)lim_r->2(∑|x_k-y_k|^r)^(1/r)
+    Parameters
+    ----------
+    point_a : int
+        Starting point
+    point_b : int
+        End point
+    Returns
+    -------
+    float
+        The distance between two points
+    """
     distance = np.linalg.norm(point_a - point_b)
     return distance
 
 
 def distance_points_array(points) -> float:
+    """
+    Euclidean distance between a list of points
+    Parameters
+    ----------
+    points : array
+        A list of points in 3D
+    Returns
+    -------
+    float
+        The distance between all points in the array
+    """
     distance = 0
     for position_point in range(len(points) - 1):
         distance += euclidian_distance_two_points(
@@ -16,6 +42,18 @@ def distance_points_array(points) -> float:
 
 
 def tortuosity_geometric_streamline(streamline_points) -> float:
+    """
+    Geometric tortuosity of a streamline
+        π=distance_points_array/distance_across
+    Parameters
+    ----------
+    streamline_points : array
+        A array of points belonging to a streamline
+    Returns
+    -------
+    float
+        Geometric tortuosity of a streamline
+    """
     distance_across = euclidian_distance_two_points(
         streamline_points[0], streamline_points[-1]
     )
@@ -23,6 +61,20 @@ def tortuosity_geometric_streamline(streamline_points) -> float:
 
 
 def tortuosity_geometric(streamlines) -> float:
+    """
+    Geometric tortuosity a list of streamlines.
+    It is the average of the geometric tortuosity for each streamline
+        π=∑tortuosity_geometric_streamline_i/lenght(streamlines)
+
+    Parameters
+    ----------
+    streamlines : array
+        A list of streamlines
+    Returns
+    -------
+    float
+        Average geometric tortuosity of all streamlines
+    """
     if len(streamlines) > 1:
         length_streamlines = len(streamlines)
         tau_geometric = 0
