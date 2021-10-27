6feb65bb546c52e8167cc276a53013a11e421b64
Joseph Avila Alvarez <josdavilalvarez@gmail.com>
2021-10-26 23:41:40 -0500

    fix normalize streamlines

--- tau_main.py
+++ tau_main.py
@@ -1,8 +1,6 @@
 from dipy.io.stateful_tractogram import StatefulTractogram
 from dipy.io.streamline import load_tractogram
 import taubrain as tb
-
-
 tracto = load_tractogram("data/data.trk", "same", bbox_valid_check=False)
 streamlines = tracto.streamlines
 print(tb.tortuosity.tortuosity_geometric_track(streamlines[0:300]))
